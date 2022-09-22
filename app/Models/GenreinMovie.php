bb543c3ca0099294312f4f30a37984aa9a7bcb6e
Nbtrien <trienbanguyen@gmail.com>
2022-09-22 10:24:49 +0700

    make it better

--- app/Models/GenreinMovie.php
+++ app/Models/GenreinMovie.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class genreinmovie extends Model
+class GenreinMovie extends Model
 {
     protected $table = 'genreinmovies';
     //
