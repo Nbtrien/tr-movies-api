3d1a9ab2b1307f5ea70ebe04eb92bef99ec7ad26
Nbtrien <trienbanguyen@gmail.com>
2022-09-23 22:00:54 +0700

    seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,28 +12,33 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(\Database\Seeds\UserSeeder::class);
-        $this->call(MoviesTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(CategoriesTableSeeder::class);
-        $this->call(CommentsTableSeeder::class);
+        $this->call(VideosTableSeeder::class);
         $this->call(CountriesTableSeeder::class);
-        $this->call(DirectorinmoviesTableSeeder::class);
+        $this->call(ImagesTableSeeder::class);
+        $this->call(TrailersTableSeeder::class);
+        $this->call(GenresTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(SeriesTableSeeder::class);
+        $this->call(CategoriesTableSeeder::class);
+        $this->call(TagsTableSeeder::class);
         $this->call(DirectorsTableSeeder::class);
+        $this->call(ActorsTableSeeder::class);
+        
+        $this->call(UsersTableSeeder::class);
+        $this->call(MoviesTableSeeder::class);
+        $this->call(SeriesmoviesTableSeeder::class);
+
+        $this->call(CommentsTableSeeder::class);
+        $this->call(DirectorinmoviesTableSeeder::class);
+        $this->call(ActorinmoviesTableSeeder::class);
         $this->call(EpisodesTableSeeder::class);
+
         $this->call(FeaturemoviesTableSeeder::class);
         $this->call(GenreinmoviesTableSeeder::class);
-        $this->call(GenresTableSeeder::class);
-        $this->call(ImagesTableSeeder::class);
         $this->call(MovieinseriesTableSeeder::class);
         $this->call(MovieintagsTableSeeder::class);
         $this->call(RatingsTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
-        $this->call(SeriesTableSeeder::class);
-        $this->call(SeriesmoviesTableSeeder::class);
-        $this->call(TagsTableSeeder::class);
-        $this->call(TrailersTableSeeder::class);
         $this->call(UsermoviesTableSeeder::class);
         $this->call(UserviewsTableSeeder::class);
-        $this->call(VideosTableSeeder::class);
     }
 }
