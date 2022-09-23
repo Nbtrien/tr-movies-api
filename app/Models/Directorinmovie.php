13d04d88e3f0ac95e00d7872b3e9a7a98420e1d5
Nbtrien <trienbanguyen@gmail.com>
2022-09-23 22:39:03 +0700

    upd

--- app/Models/Directorinmovie.php
+++ app/Models/Directorinmovie.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class DirectorinMovie extends Model
 {
+    use HasFactory;
     protected $table = 'directorinmovies';
     //
     protected $fillable = [
